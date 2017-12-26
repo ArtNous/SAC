60a22bad5f8d2c61eb7aead391b30d6f74bba76d
ArtNous <1104xyz@gmail.com>
2017-12-26 12:04:42 -0400

    No se van a crear las tablas -servicios y -clientes, ya las tiene INT

--- application/controllers/Inicio.php
+++ application/controllers/Inicio.php
@@ -2,14 +2,18 @@
 
 Class Inicio extends CI_Controller {
 
+	private $titulo = null;
+
 	public function __contruct(){
 		parent::__contruct();
 	}
 
 	public function index(){
-		$this->load->helper('url');
+		$this->titulo = 'Ingreso al sistema';
+		$datos['titulo'] = $this->titulo;
+ 		$this->load->helper('url');
 		$this->load->library('form_validation');
-		$this->load->view('main/login');
+		$this->load->view('main/login',$datos);
 	}
 
 }
